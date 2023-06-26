a726f9f3446564e25a20a08c93aec2fe2566b9aa
felixyangyicheng <felix.yang.yicheng@gmail.com>
2023-06-26 21:25:01 +0200

    conections

---
+++ TDD.Test/Connections/ConnectionFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using TDD.Data;
+
+namespace TDD.Test.Connections
+{
+	public class ConnectionFactory : IDisposable
+    {
+        #region IDisposable Support  
+        private bool disposedValue = false; // To detect redundant calls  
+
+        public BuDbContext CreateContextForInMemory()
+        {
+            var option = new DbContextOptionsBuilder<BuDbContext>().UseInMemoryDatabase(databaseName: "Test_Database").Options;
+
+            var context = new BuDbContext(option);
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+
+        public BuDbContext CreateContextForSQLite()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var option = new DbContextOptionsBuilder<BuDbContext>().UseSqlite(connection).Options;
+
+            var context = new BuDbContext(option);
+
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
+
